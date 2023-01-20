29e1cb9aee5c3df0a0dec614c6c9c94fb3d12e64
lucyTheSlayer <saber_zyl@qq.com>
2023-01-20 11:40:13 +0800

    ~

--- examples/main.go
+++ examples/main.go
@@ -3,14 +3,16 @@ package main
 import (
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
-	"fyne.io/fyne/v2/widget"
+	"fyne.io/fyne/v2/data/binding"
 	"github.com/lucyTheSlayer/fynex"
 )
 
 func main() {
 	a := app.New()
 	w := a.NewWindow("PaddingLayout")
-	text1 := widget.NewLabel("topleft")
-	w.SetContent(container.New(fynex.NewPaddingLayout(100, 10, 10, 100), text1))
+	s := binding.NewString()
+	s.Set("12.32")
+	e := fynex.NewNumericalEntryWithData(s)
+	w.SetContent(container.New(fynex.NewPaddingLayout(10, 10, 10, 10), e))
 	w.ShowAndRun()
 }
---
+++ numeric_entry.go
@@ -0,0 +1,48 @@
+package fynex
+
+import (
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/data/binding"
+	"fyne.io/fyne/v2/driver/mobile"
+	"fyne.io/fyne/v2/widget"
+	"strconv"
+)
+
+type NumericalEntry struct {
+	widget.Entry
+}
+
+func NewNumericalEntry() *NumericalEntry {
+	entry := &NumericalEntry{}
+	entry.ExtendBaseWidget(entry)
+	return entry
+}
+
+func NewNumericalEntryWithData(data binding.String) *NumericalEntry {
+	entry := NewNumericalEntry()
+	entry.Bind(data)
+	return entry
+}
+
+func (e *NumericalEntry) TypedRune(r rune) {
+	if (r >= '0' && r <= '9') || r == '.' || r == ',' {
+		e.Entry.TypedRune(r)
+	}
+}
+
+func (e *NumericalEntry) TypedShortcut(shortcut fyne.Shortcut) {
+	paste, ok := shortcut.(*fyne.ShortcutPaste)
+	if !ok {
+		e.Entry.TypedShortcut(shortcut)
+		return
+	}
+
+	content := paste.Clipboard.Content()
+	if _, err := strconv.ParseFloat(content, 64); err == nil {
+		e.Entry.TypedShortcut(shortcut)
+	}
+}
+
+func (e *NumericalEntry) Keyboard() mobile.KeyboardType {
+	return mobile.NumberKeyboard
+}
